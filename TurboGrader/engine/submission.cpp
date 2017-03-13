fafac398e6a9031ab284815b230cfe84819b2e53
gharbiw <gharbiw@lafayette.edu>
2017-03-13 19:54:01 -0400

    Added submission class

---
+++ TurboGrader/engine/submission.cpp
@@ -0,0 +1,6 @@
+#include "submission.h"
+
+Submission::Submission()
+{
+
+}
---
+++ TurboGrader/engine/submission.h
@@ -0,0 +1,11 @@
+#ifndef SUBMISSION_H
+#define SUBMISSION_H
+
+
+class Submission
+{
+public:
+    Submission();
+};
+
+#endif // SUBMISSION_H
