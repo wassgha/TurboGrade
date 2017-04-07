81e93820dc834f2cac1422d9a6ef4da4e657eac8
nortondj <nortondj@lafayette.edu>
2017-04-06 22:37:30 -0400

    Add first pass CSVGenerator interface

--- TurboGrade/tools/csvgenerator.h
+++ TurboGrade/tools/csvgenerator.h
@@ -1,11 +1,16 @@
 #ifndef CSVGENERATOR_H
 #define CSVGENERATOR_H
-
+#include "../engine/controller.h"
+#include "filewriter.h"
+#include <iostream>
 
 class CSVGenerator : public ObjectIdentifier
 {
 public:
     CSVGenerator();
+
+    static void printProfessor(Section *section, Assignment *assignment,
+                               QString filepath);
 };
 
 #endif // CSVGENERATOR_H
