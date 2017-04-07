8fed3bc517cc31afb45a43a25c38fec244048a6e
nortondj <nortondj@lafayette.edu>
2017-04-06 22:40:23 -0400

    CSVGenerator iterates through Criteria in correct order

--- TurboGrade/tools/csvgenerator.cpp
+++ TurboGrade/tools/csvgenerator.cpp
@@ -4,3 +4,15 @@ CSVGenerator::CSVGenerator()
 {
 
 }
+
+void CSVGenerator::printProfessor(Section *section, Assignment *assignment, QString filepath){
+    FileWriter h(filepath.toStdString());
+    std::string criteria ="";
+    for(Criterion *crit: *assignment->_rubric->_criteria){
+        criteria.append(crit->to_string());
+        criteria.append(",");
+    }
+    std::string header = "Student,";
+    header.append(criteria);
+    std::cerr << header;
+}
--- TurboGrade/tools/main.cpp
+++ TurboGrade/tools/main.cpp
@@ -2,6 +2,8 @@
 #include <QApplication>
 #include "htmltopdf.h"
 #include "macadress.h"
+#include "../engine/controller.h"
+#include "csvgenerator.h"
 using namespace std;
 
 int main(int argc, char *argv[])
@@ -13,5 +15,14 @@ int main(int argc, char *argv[])
     cout << "Hello World!" << endl;
 
     MacAdress m;
+    Controller *_controller;
+
+    // Use new Controller(true) to drop tables
+    _controller = new Controller();
+    Course *course = _controller->get_course("CS 105");
+    Section *section = course->get_section("02");
+    Assignment* assignment = section->get_assignment("Binary Search Tree");
+    CSVGenerator g;
+    g.printProfessor(section,assignment,"test.txt");
     return 0;
 }
