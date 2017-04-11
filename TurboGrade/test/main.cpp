b33096e5ba6afc59e984c833af9923baec1adab1
Sam Cutrone <cutrones@lafayette.edu>
2017-04-11 11:25:39 -0400

    next set of tests

--- TurboGrade/test/main.cpp
+++ TurboGrade/test/main.cpp
@@ -84,31 +84,39 @@ TEST(general,CONTROLLERBReplaceLose) {
     Controller* newControl = new Controller(true,"test.sql");
     vector<Course*>* courseList = newControl->get_courses();
     int x = courseList->size();
-    cout<<to_string(x)<<endl;
     ASSERT_EQ(x,0);
 }
 
 TEST(general,2CONTROLLERBUILD) {
-    Controller testControl;
-    testControl.add_course(0,"testCourse");
-    vector<Course*>* courseList = testControl.get_courses();
-    int x = courseList->size();
-    ASSERT_EQ(x,1);
-    testControl.add_course(1,"test2");
-    courseList = testControl.get_courses();
-    int x2 = courseList->size();
+    Controller* testControl = new Controller(false,"test.sql");
+    testControl->add_course(-1,"testCourse");
+    vector<Course*>* courseList = testControl->get_courses();
+    Course* testCourse = courseList->at(0);
+    testCourse->add_section(-1,"testSection");
+    testCourse->add_section(-1,"testSection2");
+    delete testControl;
+    Controller* newControl = new Controller(false,"test.sql");
+    vector<Course*>* newCourseList = newControl->get_courses();
+    Course* newTestCourse = newCourseList->at(0);
+    vector<Section*>* sectionList = newTestCourse->get_sections();
+    int x = sectionList->size();
     cout<<to_string(x)<<endl;
-    ASSERT_EQ(x2,2);
+    ASSERT_EQ(x,2);
 }
 
 TEST(general,2CONTROLLERBReplaceKeep) {
     Controller* testControl = new Controller(false,"test.sql");
     testControl->add_course(-1,"testCourse");
-    testControl->add_course(-1,"Course2");
+    vector<Course*>* courseList = testControl->get_courses();
+    Course* testCourse = courseList->at(0);
+    testCourse->add_section(-1,"testSection");
+    testCourse->add_section(-1,"testSection2");
     delete testControl;
     Controller* newControl = new Controller(false,"test.sql");
-    vector<Course*>* courseList = newControl->get_courses();
-    int x = courseList->size();
+    vector<Course*>* newCourseList = newControl->get_courses();
+    Course* newTestCourse = newCourseList->at(0);
+    vector<Section*>* sectionList = newTestCourse->get_sections();
+    int x = sectionList->size();
     cout<<to_string(x)<<endl;
     ASSERT_EQ(x,2);
 }
