3609db577d5e77228c6bd65680994e092aa37f8e
Sam Cutrone <cutrones@lafayette.edu>
2017-04-11 11:36:40 -0400

    fixing tests

--- TurboGrade/test/main.cpp
+++ TurboGrade/test/main.cpp
@@ -50,14 +50,15 @@ int main(int argc, char *argv[])
     return RUN_ALL_TESTS();
 }
 
+//course addition/not
 
 TEST(general,CONTROLLERBUILD) {
-    Controller testControl;
-    testControl.add_course(0,"testCourse");
+    Controller testControl(true,"test.sql");
+    testControl.add_course(-1,"testCourse");
     vector<Course*>* courseList = testControl.get_courses();
     int x = courseList->size();
     ASSERT_EQ(x,1);
-    testControl.add_course(1,"test2");
+    testControl.add_course(-1,"test2");
     courseList = testControl.get_courses();
     int x2 = courseList->size();
     cout<<to_string(x)<<endl;
@@ -65,7 +66,7 @@ TEST(general,CONTROLLERBUILD) {
 }
 
 TEST(general,CONTROLLERBReplaceKeep) {
-    Controller* testControl = new Controller(false,"test.sql");
+    Controller* testControl = new Controller(true,"test.sql");
     testControl->add_course(-1,"testCourse");
     testControl->add_course(-1,"Course2");
     delete testControl;
@@ -77,7 +78,7 @@ TEST(general,CONTROLLERBReplaceKeep) {
 }
 
 TEST(general,CONTROLLERBReplaceLose) {
-    Controller* testControl = new Controller(false,"test.sql");
+    Controller* testControl = new Controller(true,"test.sql");
     testControl->add_course(-1,"testCourse");
     testControl->add_course(-1,"Course2");
     delete testControl;
@@ -87,25 +88,28 @@ TEST(general,CONTROLLERBReplaceLose) {
     ASSERT_EQ(x,0);
 }
 
+//section addition/not
+
 TEST(general,2CONTROLLERBUILD) {
-    Controller* testControl = new Controller(false,"test.sql");
+    Controller* testControl = new Controller(true,"test.sql");
     testControl->add_course(-1,"testCourse");
     vector<Course*>* courseList = testControl->get_courses();
     Course* testCourse = courseList->at(0);
     testCourse->add_section(-1,"testSection");
-    testCourse->add_section(-1,"testSection2");
-    delete testControl;
-    Controller* newControl = new Controller(false,"test.sql");
-    vector<Course*>* newCourseList = newControl->get_courses();
-    Course* newTestCourse = newCourseList->at(0);
-    vector<Section*>* sectionList = newTestCourse->get_sections();
+    vector<Section*>* sectionList = testCourse->get_sections();
     int x = sectionList->size();
+    ASSERT_EQ(x,1);
+    testCourse->add_section(-1,"testSection2");
+    courseList = testControl->get_courses();
+    testCourse = courseList->at(0);
+    vector<Section*>* sectionList2 = testCourse->get_sections();
+    int y = sectionList2->size();
     cout<<to_string(x)<<endl;
-    ASSERT_EQ(x,2);
+    ASSERT_EQ(y,2);
 }
 
 TEST(general,2CONTROLLERBReplaceKeep) {
-    Controller* testControl = new Controller(false,"test.sql");
+    Controller* testControl = new Controller(true,"test.sql");
     testControl->add_course(-1,"testCourse");
     vector<Course*>* courseList = testControl->get_courses();
     Course* testCourse = courseList->at(0);
@@ -122,7 +126,7 @@ TEST(general,2CONTROLLERBReplaceKeep) {
 }
 
 TEST(general,2CONTROLLERBReplaceLose) {
-    Controller* testControl = new Controller(false,"test.sql");
+    Controller* testControl = new Controller(true,"test.sql");
     testControl->add_course(-1,"testCourse");
     vector<Course*>* courseList = testControl->get_courses();
     Course* testCourse = courseList->at(0);
@@ -137,6 +141,8 @@ TEST(general,2CONTROLLERBReplaceLose) {
     cout<<to_string(x)<<endl;
     ASSERT_EQ(x,2);
 }
+
+//assignment addition/not
 
 TEST(general,3CONTROLLERBUILD) {
     Controller testControl;
