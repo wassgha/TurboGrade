e00963274d04db7679d11097541255dd9840e3b8
nortondj <nortondj@lafayette.edu>
2017-04-26 03:29:14 -0400

    Add dump tool headers

--- TurboGrade/tools/dumptool.h
+++ TurboGrade/tools/dumptool.h
@@ -1,11 +1,16 @@
 #ifndef DUMPTOOL_H
 #define DUMPTOOL_H
-
+#include <QString>
+#include <QDebug>
+#include <QProcess>
 
 class DumpTool
 {
 public:
     DumpTool();
+    void run_process(QString command, QString inputFile = "", QString outputFile = "");
+    void dump(QString dbname, QString outputFile);
+    void restore(QString dbname, QString inputFile);
 };
 
 #endif // DUMPTOOL_H
