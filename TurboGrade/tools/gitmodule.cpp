6bc5109595320a37dbcc0875b2ba5e6c66fe9230
nortondj <nortondj@lafayette.edu>
2017-04-26 20:29:18 -0400

    Add implementation for git add

--- TurboGrade/tools/gitmodule.cpp
+++ TurboGrade/tools/gitmodule.cpp
@@ -53,3 +53,52 @@ void GitModule::clone(QString workingDirectory, QString repoUrl){
         qDebug() << "Finished ok";
     }
 }
+
+/**
+ * @brief GitModule::add adds the fileToAdd to the repository located in working
+ * Directory
+ * @param workingDirectory the workingDirectory of the repo
+ * @param fileToAdd the file/regex to be added
+ */
+void GitModule::add(QString workingDirectory, QString fileToAdd){
+    // create process
+    QProcess process;
+    process.setProcessChannelMode(QProcess::ForwardedChannels);
+    // add some arguments
+    QStringList arguments;
+    process.setWorkingDirectory(workingDirectory);
+    // arguments are add and the fileToAdd
+    arguments << "add" << fileToAdd;
+
+    qDebug() << "Process trying to run command : git add " << repoUrl;
+    qDebug() << "In : " << process.workingDirectory();
+
+    // start process git with args add fileToAdd
+    // git add fileToAdd
+    process.start("git", arguments);
+    if(process.waitForFinished()){
+        // get standard out string
+        QString out = process.readAllStandardOutput();
+        // get error string
+        QString error = process.readAllStandardError();
+        if(!out.isEmpty()){
+            qDebug() << "Standard Out: ";
+            QStringList lines = out.split("\n");
+            for(QString line : lines){
+                qDebug() << line;
+            }
+        }
+        if(!error.isEmpty()){
+            qDebug() << "Errors: ";
+            lines = error.split("\n");
+            for(QString line : lines){
+                qDebug() << line;
+            }
+        }
+    }
+
+    QProcess::ExitStatus Status = process.exitStatus();
+    if(Status == 0){
+        qDebug() << "Finished ok";
+    }
+}
--- TurboGrade/tools/gitmodule.h
+++ TurboGrade/tools/gitmodule.h
@@ -10,7 +10,7 @@ public:
     // clone the Git repo to the workingDirectory
     static void clone(QString workingDirectory, QString repoUrl);
     // add the file/directory/regex to the Git Repo in the workingDirectory
-    static void add(QString workingDirectory, QString fileToCommit);
+    static void add(QString workingDirectory, QString fileToAdd);
     // commit in the workingDirectory with message
     static void commit(QString workingDirectory, QString message);
     // push the repo in the workingDirectory
