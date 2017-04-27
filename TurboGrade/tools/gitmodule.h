2f0a39fce31c4b16c17979bc57a75f21748755c0
nortondj <nortondj@lafayette.edu>
2017-04-26 20:22:13 -0400

    Add gitmodule method headers to define API

--- TurboGrade/tools/gitmodule.h
+++ TurboGrade/tools/gitmodule.h
@@ -1,11 +1,22 @@
 #ifndef GITMODULE_H
 #define GITMODULE_H
-
+#include <QString>
+#include <QProcess>
 
 class GitModule
 {
 public:
     GitModule();
+    // clone the Git repo to the workingDirectory
+    static void clone(QString workingDirectory, QString repoUrl);
+    // add the file/directory/regex to the Git Repo in the workingDirectory
+    static void add(QString workingDirectory, QString fileToCommit);
+    // commit in the workingDirectory with message
+    static void commit(QString workingDirectory, QString message);
+    // push the repo in the workingDirectory
+    static void push(QString workingDirectory);
+    // pull from origin
+    static void pull(QString workingDirectory);
 };
 
 #endif // GITMODULE_H
