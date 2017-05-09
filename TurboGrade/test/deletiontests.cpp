e68b447b4b0987b2f24d34c0617f262ededa7aea
nortondj <nortondj@lafayette.edu>
2017-05-09 02:39:16 -0400

    Add tests for parent and sub criterion removal regards to grades

--- TurboGrade/test/deletiontests.cpp
+++ TurboGrade/test/deletiontests.cpp
@@ -76,4 +76,106 @@ TEST_F(DeletionTests, RemoveCriterionRemovesComments2){
     ASSERT_TRUE(comments.empty());
 }
 
+TEST_F(DeletionTests, RemoveCriterionUpdatesSubmissionGrade){
+    testRubric->remove_criterion(testCriterion);
+    ASSERT_EQ(testSubmission->get_grade(), 0);
+}
+
+TEST_F(DeletionTests, RemoveCriterionUpdatesRubricGrade){
+    testRubric->remove_criterion(testCriterion);
+    ASSERT_EQ(testRubric->total_grade(), 0);
+}
+
+TEST_F(DeletionTests, RemoveParentCriterionRemovesFromRubric){
+    Criterion* subCrit = testRubric->add_criterion("SubCrit1",testCriterion,2);
+    testSubmission->add_comment("File", subCrit, "nice", -1, 0, 0);
+    testRubric->remove_criterion(testCriterion);
+    ASSERT_EQ(testRubric->get_criterion("SubCrit1"), nullptr);
+}
+
+TEST_F(DeletionTests, RemoveParentCriterionRemovesFromRubricObject){
+    Criterion* subCrit = testRubric->add_criterion("SubCrit1",testCriterion,2);
+    testSubmission->add_comment("File", subCrit, "nice", -1, 0, 0);
+    testRubric->remove_criterion(testCriterion);
+    std::vector<Criterion*> criteria = *testRubric->getCriterions();
+    ASSERT_FALSE(std::find(criteria.begin(), criteria.end(), subCrit) != criteria.end());
+}
+
+TEST_F(DeletionTests, RemoveParentCriterionRemovesComments1){
+    Criterion* subCrit = testRubric->add_criterion("SubCrit1",testCriterion,2);
+    testSubmission->add_comment("File", subCrit, "nice", -1, 0, 0);
+    testRubric->remove_criterion(testCriterion);
+    std::vector<Comment *> comments = testSubmission->get_comments(subCrit);
+    ASSERT_FALSE(std::find(comments.begin(), comments.end(), testComment) != comments.end());
+}
+
+TEST_F(DeletionTests, RemoveParentCriterionRemovesComments2){
+    Criterion* subCrit = testRubric->add_criterion("SubCrit1",testCriterion,2);
+    testSubmission->add_comment("File", subCrit, "nice", -1, 0, 0);
+    testRubric->remove_criterion(testCriterion);
+    std::vector<Comment*> comments = *testSubmission->_comments;
+    ASSERT_TRUE(comments.empty());
+}
+
+TEST_F(DeletionTests, RemoveParentCriterionUpdatesSubmissionGrade){
+    Criterion* subCrit = testRubric->add_criterion("SubCrit1",testCriterion,2);
+    testSubmission->add_comment("File", subCrit, "nice", -1, 0, 0);
+    testRubric->remove_criterion(testCriterion);
+    ASSERT_EQ(testSubmission->get_grade(), 0);
+}
+
+TEST_F(DeletionTests, RemoveParentCriterionUpdatesRubricGrade){
+    Criterion* subCrit = testRubric->add_criterion("SubCrit1",testCriterion,2);
+    testSubmission->add_comment("File", subCrit, "nice", -1, 0, 0);
+    testRubric->remove_criterion(testCriterion);
+    ASSERT_EQ(testRubric->total_grade(), 0);
+}
+
+TEST_F(DeletionTests, RemoveSubCriterionRemovesFromRubric){
+    Criterion* subCrit = testRubric->add_criterion("SubCrit1",testCriterion,2);
+    testSubmission->add_comment("File", subCrit, "nice", -1, 0, 0);
+    testRubric->remove_criterion(subCrit);
+    ASSERT_EQ(testRubric->get_criterion("SubCrit1"), nullptr);
+}
+
+TEST_F(DeletionTests, RemoveSubCriterionRemovesFromRubricObject){
+    Criterion* subCrit = testRubric->add_criterion("SubCrit1",testCriterion,2);
+    testSubmission->add_comment("File", subCrit, "nice", -1, 0, 0);
+    testRubric->remove_criterion(subCrit);
+    std::vector<Criterion*> criteria = *testRubric->getCriterions();
+    ASSERT_FALSE(std::find(criteria.begin(), criteria.end(), subCrit) != criteria.end());
+}
+
+TEST_F(DeletionTests, RemoveSubCriterionRemovesComments1){
+    Criterion* subCrit = testRubric->add_criterion("SubCrit1",testCriterion,2);
+    Comment* subComment = testSubmission->add_comment("File", subCrit, "nice", -1, 0, 0);
+    testRubric->remove_criterion(subCrit);
+    std::vector<Comment *> comments = testSubmission->get_comments(subCrit);
+    ASSERT_FALSE(std::find(comments.begin(), comments.end(), subComment) != comments.end());
+}
+
+TEST_F(DeletionTests, RemoveSubCriterionRemovesOnlyCommentsFromSub){
+    Criterion* subCrit = testRubric->add_criterion("SubCrit1",testCriterion,2);
+    testSubmission->add_comment("File", subCrit, "nice", -1, 0, 0);
+    testRubric->remove_criterion(subCrit);
+    std::vector<Comment*> comments = *testSubmission->_comments;
+    ASSERT_EQ(comments.size(), 1);
+}
+
+TEST_F(DeletionTests, RemoveSubCriterionUpdatesSubmissionGrade){
+    Criterion* subCrit = testRubric->add_criterion("SubCrit1",testCriterion,2);
+    testSubmission->add_comment("File", subCrit, "nice", 1,0,0);
+    ASSERT_EQ(testSubmission->get_grade(testCriterion), 4);
+    testRubric->remove_criterion(subCrit);
+    ASSERT_EQ(testSubmission->get_grade(testCriterion), 3);
+}
+
+TEST_F(DeletionTests, RemoveSubCriterionUpdatesRubricGrade){
+    Criterion* subCrit = testRubric->add_criterion("SubCrit1",testCriterion,2);
+    testSubmission->add_comment("File", subCrit, "nice", -1, 0, 0);
+    testRubric->remove_criterion(subCrit);
+    ASSERT_EQ(testRubric->total_grade(), 10);
+}
+
+
 
