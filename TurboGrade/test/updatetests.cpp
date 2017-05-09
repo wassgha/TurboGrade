11ba4b1736accdcf59b9cd5f58516cbef5e7382f
nortondj <nortondj@lafayette.edu>
2017-05-09 04:38:29 -0400

    Add UpdateTests class headers, setup, and teardown implementation

--- TurboGrade/test/updatetests.cpp
+++ TurboGrade/test/updatetests.cpp
@@ -1,6 +1,27 @@
 #include "updatetests.h"
 
-UpdateTests::UpdateTests()
-{
+UpdateTests::UpdateTests(){
 
 }
+
+UpdateTests::~UpdateTests(){
+
+}
+
+void UpdateTests::SetUp(){
+    testControl = new Controller(true,"test.sql");
+    testCourse = testControl->add_course("testCourse", "Fall 2017");
+    testSection = testCourse->add_section("testSection");
+    testStudent = testSection->add_student("testStudent",-1);
+    testAssignment = new Assignment("Test","Objective",testControl,false,-1);
+    testSection->add_assignment(testAssignment);
+    testRubric = new Rubric(testAssignment,testControl);
+    testCriterion = testRubric->add_criterion("testCriterion",nullptr,10,-1);
+    testSubmission = testStudent->add_submission(testAssignment);
+    testComment = testSubmission->add_comment("file",testCriterion,"text",3,0,10,-1);
+}
+
+void UpdateTests::TearDown(){
+    delete testControl;
+}
+
