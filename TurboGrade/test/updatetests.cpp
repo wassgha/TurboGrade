13f3a209a57b97c498a6f50f6a548a5d8152d478
nortondj <nortondj@lafayette.edu>
2017-05-09 04:48:12 -0400

    Add unit tests for editing comments

--- TurboGrade/test/updatetests.cpp
+++ TurboGrade/test/updatetests.cpp
@@ -25,3 +25,34 @@ void UpdateTests::TearDown(){
     delete testControl;
 }
 
+TEST_F(UpdateTests, UpdateCommentText){
+    testComment->update("File", testCriterion, "New Text", 3, 0, 10);
+    ASSERT_EQ(0, testComment->_text.compare("New Text"));
+}
+
+TEST_F(UpdateTests, UpdateCommentCriterion){
+    Criterion *newCrit = testRubric->add_criterion("NewCrit", nullptr, 5);
+    testComment->update("File", newCrit, "text", 3, 0, 10);
+    ASSERT_EQ(newCrit, testComment->_criterion);
+}
+
+TEST_F(UpdateTests, UpdateCommentFile){
+    testComment->update("New File", testCriterion, "text", 3, 0, 10);
+    ASSERT_EQ(0, testComment->_filename.compare("New File"));
+}
+
+TEST_F(UpdateTests, UpdateCommentGrade){
+    testComment->update("File", testCriterion, "text", 1, 0, 10);
+    ASSERT_EQ(1, testComment->_grade);
+}
+
+TEST_F(UpdateTests, UpdateCommentStartPos){
+    testComment->update("File", testCriterion, "text", 3, 5, 10);
+    ASSERT_EQ(5, testComment->_start_pos);
+}
+
+TEST_F(UpdateTests, UpdateCommentEndPost){
+    testComment->update("File", testCriterion, "text", 3, 0, 15);
+    ASSERT_EQ(15, testComment->_end_pos);
+}
+
